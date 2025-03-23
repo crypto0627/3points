d8820e264849bce14f9a11c3a6f5cd85d94dafd5
crypto0627 <jake0627a1@gmail.com>
2025-03-23 18:20:36 +0800

    feat: simulate action

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,7 +1,7 @@
 import { PayBlock } from "@/components/Pay";
 import { SignIn } from "@/components/SignIn";
 import { VerifyBlock } from "@/components/Verify";
-import { SimulateVerify } from "@/components/simulate_verify";
+import SimulateVerify from "@/components/simulate_verify";
 
 export default function Home() {
   return (
--- apps/frontend/components/simulate_verify.tsx
+++ apps/frontend/components/simulate_verify.tsx
@@ -1,38 +1,61 @@
-'use client' // for Next.js app router
-import { IDKitWidget, VerificationLevel, ISuccessResult } from '@worldcoin/idkit'
+"use client";
 
-const handleVerify = async (proof: ISuccessResult) => {
-  const res = await fetch("/api/verify-simulate", { // route to your backend will depend on implementation
-      method: "POST",
-      headers: {
-          "Content-Type": "application/json",
-      },
-      body: JSON.stringify(proof),
-  })
-  if (!res.ok) {
-      throw new Error("Verification failed."); // IDKit will display the error message to the user in the modal
+import { VerificationLevel, IDKitWidget, useIDKit } from "@worldcoin/idkit";
+import type { ISuccessResult } from "@worldcoin/idkit";
+import { verify } from "@/app/actions/verify";
+
+export default function SimulateVerify() {
+  const app_id = process.env.APP_ID as `app_${string}`;
+  const action = "hello-world" as string;
+
+  if (!app_id) {
+    throw new Error("app_id is not set in environment variables!");
   }
-};
+  if (!action) {
+    throw new Error("action is not set in environment variables!");
+  }
 
-const onSuccess = () => {
-  // This is where you should perform any actions after the modal is closed
-  // Such as redirecting the user to a new page
-  window.location.href = "/success";
-};
+  const { setOpen } = useIDKit();
 
-export const SimulateVerify = () => {
+  const onSuccess = (result: ISuccessResult) => {
+    // This is where you should perform frontend actions once a user has been verified, such as redirecting to a new page
+    window.alert(
+      "Successfully verified with World ID! Your nullifier hash is: " +
+        result.nullifier_hash
+    );
+  };
+
+  const handleProof = async (result: ISuccessResult) => {
+    console.log(
+      "Proof received from IDKit, sending to backend:\n",
+      JSON.stringify(result)
+    ); // Log the proof from IDKit to the console for visibility
+    const data = await verify(result);
+    if (data.success) {
+      console.log("Successful response from backend:\n", JSON.stringify(data)); // Log the response from our backend for visibility
+    } else {
+      throw new Error(`Verification failed: ${data.detail}`);
+    }
+  };
+
   return (
-    <IDKitWidget
-      app_id={`app_${process.env.APP_ID}`} // obtained from the Developer Portal
-	action="hello-world" // obtained from the Developer Portal
-	onSuccess={onSuccess} // callback when the modal is closed
-	handleVerify={handleVerify} // callback when the proof is received
-	verification_level={VerificationLevel.Orb}
->
-      {({ open }) => (
-        // This is the button that will open the IDKit modal
-        <button onClick={open}>Verify with World ID</button>
-      )}
-    </IDKitWidget>
+    <div>
+      <div className="flex flex-col items-center justify-center align-middle h-screen">
+        <p className="text-2xl mb-5">World ID Cloud Template</p>
+        <IDKitWidget
+          action={action}
+          app_id={app_id}
+          onSuccess={onSuccess}
+          handleVerify={handleProof}
+          verification_level={VerificationLevel.Orb} // Change this to VerificationLevel.Device to accept Orb- and Device-verified users
+        />
+        <button
+          className="border border-black rounded-md"
+          onClick={() => setOpen(true)}
+        >
+          <div className="mx-3 my-1">Verify with World ID</div>
+        </button>
+      </div>
+    </div>
   );
-};
+}
